b988c84ec4740737af09a346f4e84a6b7065a430
Slcastro45 <slcastr45@misena.edu.co>
2020-10-14 11:28:16 -0500

    Se agregaron las migraciones de las tablas detalle

---
+++ CarpetaProyecto/database/migrations/2020_10_14_161515_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('idProveedor')->unsigned();
+            $table->integer('idIva')->unsigned();
+            $table->string('nombre', 45);
+            $table->double('precio', 11, 2);
+            $table->boolean('estado')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
