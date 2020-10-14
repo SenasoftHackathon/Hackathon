b01fba818ec475fd1be8ef0044c266dd2055d207
Slcastro45 <slcastr45@misena.edu.co>
2020-10-14 09:54:34 -0500

    Se agregaron las migraciones de las tablas Sucursal, Rol y Permiso

---
+++ database/migrations/2020_10_14_143424_create_sucursal_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSucursalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sucursal', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre', 40);
+            $table->string('direccion', 60);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sucursal');
+    }
+}
---
+++ database/migrations/2020_10_14_143801_create_permiso_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermisoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permiso', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombrePermiso', 20);
+            $table->string('descripcion', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permiso');
+    }
+}
