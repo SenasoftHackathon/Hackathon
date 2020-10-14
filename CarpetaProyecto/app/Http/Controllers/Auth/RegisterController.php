6cbaa36a717fd3f5f448a57225ec2c2159d9bc40
NosoyCristian <ccmunoz481@misena.edu.co>
2020-10-14 16:48:36 -0500

    configuración login y registro

--- CarpetaProyecto/app/Http/Controllers/Auth/RegisterController.php
+++ CarpetaProyecto/app/Http/Controllers/Auth/RegisterController.php
@@ -64,6 +64,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            'idSucursal' => $data['sucursal'],
+            'idRol' => $data['rol'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
